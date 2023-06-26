4ce4f90a1b2b0deeb3428c8425050cf6bf470e4e
Igo Pinheiro <igo.pinheiro1@gmail.com>
2023-06-26 11:23:41 -0400

    Feat: Desenvolvimento Discount Api MicroServices - com PostgreSQL + Dapper

---
+++ Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,36 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.API.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class DiscountController : ControllerBase
+{
+    private readonly IDiscountRepository _repository;
+
+    public DiscountController(IDiscountRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet("{productName}", Name = "GetDiscount")]
+    public async Task<IActionResult> Get(string productName)
+        => Ok(await _repository.GetDiscount(productName));
+
+    [HttpPost(Name ="CreateDiscount")]
+    public async Task<IActionResult> Post([FromBody] Coupon coupon)
+    {
+        await _repository.CreateDiscount(coupon);
+        return CreatedAtAction("GetDiscount", new { productName = coupon.ProductName});
+    }
+
+    [HttpPut(Name ="UpdateDiscount")]
+    public async Task<IActionResult> Put([FromBody] Coupon coupon)
+        => Ok(await _repository.UpdateDiscount(coupon));
+
+    [HttpDelete("{productName}",Name ="DeleteDiscount")]
+    public async Task<IActionResult> Delete(string productName)
+        => Ok(await _repository.DeleteDiscount(productName));
+}
---
+++ Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,83 @@
+﻿using Dapper;
+using Discount.API.Entities;
+using Npgsql;
+
+namespace Discount.API.Repositories;
+
+public class DiscountRepository : IDiscountRepository
+{
+    private readonly IConfiguration _configuration;
+
+    public DiscountRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<bool> CreateDiscount(Coupon coupon)
+    {
+        using NpgsqlConnection connection = GetConnectionPostgreSQL();
+
+        var affected = await connection.ExecuteAsync(
+            "INSERT INTO Coupon (ProductName, Description, Amount)" +
+            "VALUES (@ProductName,@Description,@Amount)",
+            new {ProductName = coupon.ProductName,
+                 Description = coupon.Description,
+                 Amount = coupon.Amount});
+
+        return !(affected == 0);
+    }
+
+    public async Task<bool> DeleteDiscount(string productName)
+    {
+
+        using NpgsqlConnection connection = GetConnectionPostgreSQL();
+
+        var affected = await connection.ExecuteAsync(
+            "DELETE FROM Coupon" +
+            "WHERE ProductName = @ProductName",
+            new
+            {
+                ProductName = productName,
+            });
+
+        return !(affected == 0);
+    }
+
+    public async Task<Coupon> GetDiscount(string productName)
+    {
+        using NpgsqlConnection connection = GetConnectionPostgreSQL();
+
+        var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
+            ("SELECT * FROM Coupon WHERE ProductName = @ProductName",
+            new { ProductName = productName});
+
+        return coupon == null
+            ? new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount" }
+            : coupon;
+    }
+   
+    public async Task<bool> UpdateDiscount(Coupon coupon)
+    {
+        using NpgsqlConnection connection = GetConnectionPostgreSQL();
+
+        var affected = await connection.ExecuteAsync(
+            "UPDATE Coupon SET ProductName = @ProductName," +
+            "           Description = @Description," +
+            "           Amount = @Amount" +
+            "WHERE Id = @Id",
+            new
+            {
+                ProductName = coupon.ProductName,
+                Description = coupon.Description,
+                Amount = coupon.Amount,
+                Id = coupon.Id
+            });
+
+        return !(affected == 0);
+    }
+
+    private NpgsqlConnection GetConnectionPostgreSQL()
+    {
+        return new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+    }
+}
