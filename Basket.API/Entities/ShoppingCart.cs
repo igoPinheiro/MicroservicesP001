abbba2277f3b6902e0b7abd32b5a33f18e6ca3bf
Igo Pinheiro <igo.pinheiro1@gmail.com>
2023-06-25 21:59:39 -0400

    refact: Corrigir bug TotalPrice quando Items está nulo

--- Basket.API/Entities/ShoppingCart.cs
+++ Basket.API/Entities/ShoppingCart.cs
@@ -8,7 +8,7 @@ public class ShoppingCart
     }
 
     public string UserName { get; set; } = string.Empty;
-    public List<ShoppingCartItem> Items { get; set; }
+    public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
 
     public decimal TotalPrice
     {
